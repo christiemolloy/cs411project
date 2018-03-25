c4a43d67ffb9bb44d3964d7ed5faf3ebfc4550ea
stephshin2 <stephshin2@dhcp-wifi-8021x-155-41-18-37.bu.edu>
2018-03-25 13:48:02 -0400

    image page created

--- CapThat/routes/index.js
+++ CapThat/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CapThat' });
 });
+
+/* GET image page. */
+router.get('/image', function(req, res, next) {
+    res.render('image', { title: 'Upload image' });
+});
+
 
 module.exports = router;
