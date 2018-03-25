e5accdd22d4a94546e3cdf9b60ae276abab5755c
Deborah He <deborahhe@dhcp-wifi-8021x-168-122-219-155.bu.edu>
2018-03-25 13:57:59 -0400

    added clarifai.js

---
+++ CapThat/routes/clarifai.js
@@ -0,0 +1,14 @@
+const Clarifai = require("clarifai");
+const app = new Clarifai.App({
+ apiKey: 'fe2b42b4d83e4f01ababb8c56ccdd08d'
+});
+
+// predict the contents of an image by passing in a url
+app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+  function(response) {
+    console.log(JSON.stringify(response));
+  },
+  function(err) {
+    console.error(err);
+  }
+);
